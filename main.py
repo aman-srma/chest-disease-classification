d9a2c391705007b3f3708c86010b95fb01e4e952
aman-srma <aman.srma.a7@gmail.com>
2025-01-23 09:55:27 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import (DataIngestion, DataIngestionTrainingPipeline)
 
-logger.info("Welcome To cnnClassifier!")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
